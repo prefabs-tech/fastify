46eb14c83c3785edd452ce595bf689bafb607ac4
anvesh <anvesh@dzangolab.com>
2025-07-29 18:12:04 +0545

    feat: export plugin with necessary types

--- packages/errorHandler/src/index.ts
+++ packages/errorHandler/src/index.ts
@@ -1,18 +1,11 @@
-import type { ApiConfig } from "./types";
+import type { HttpErrors } from "@fastify/sensible";
 
 declare module "fastify" {
   interface FastifyInstance {
-    config: ApiConfig;
-    hostname: string;
-  }
-
-  interface FastifyRequest {
-    config: ApiConfig;
+    httpErrors: HttpErrors;
   }
 }
 
 export { default } from "./plugin";
 
-export { default as parse } from "./parse";
-
-export type { ApiConfig, AppConfig } from "./types";
+export type { ErrorResponse } from "./types";

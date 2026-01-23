605f1fecec1a2e1f7a2b597b2c964682d58d718b
Ramesh <ramesh@dzangolab.com>
2026-01-23 15:00:34 +0545

    chore: pass only s3 config to the s3Client constructor

--- packages/s3/src/utils/s3Client.ts
+++ packages/s3/src/utils/s3Client.ts
@@ -13,20 +13,20 @@ import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 import { convertStreamToBuffer } from ".";
 
+import type { S3Config } from "../types";
 import type {
   AbortMultipartUploadCommandOutput,
   CompleteMultipartUploadCommandOutput,
   DeleteObjectCommandOutput,
   ListObjectsCommandOutput,
 } from "@aws-sdk/client-s3";
-import type { ApiConfig } from "@prefabs.tech/fastify-config";
 
 class s3Client {
   protected _bucket: string = undefined as unknown as string;
-  protected _config: ApiConfig;
+  protected _config: S3Config;
   protected _storageClient: S3Client;
 
-  constructor(config: ApiConfig) {
+  constructor(config: S3Config) {
     this._config = config;
     this._storageClient = this.init();
   }
@@ -174,7 +174,7 @@ class s3Client {
   }
 
   protected init(): S3Client {
-    return new S3Client(this.config.s3.clientConfig);
+    return new S3Client(this.config.clientConfig);
   }
 }
 

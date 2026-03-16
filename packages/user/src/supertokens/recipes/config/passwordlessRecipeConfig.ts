dc2061451e0a6e197bb6177303740ac8c227940d
samarajya <samarajya@dzangolab.com>
2026-03-16 16:27:56 +0545

    feat: use twilio service for sms delivery

--- packages/user/src/supertokens/recipes/config/passwordlessRecipeConfig.ts
+++ packages/user/src/supertokens/recipes/config/passwordlessRecipeConfig.ts
@@ -1,7 +1,9 @@
 import { FastifyInstance } from "fastify";
+import { TwilioService } from "supertokens-node/recipe/passwordless/smsdelivery";
 
 import { PasswordlessRecipe } from "src/supertokens/types/passwordlessRecipe";
 
+import type { TwilioServiceConfig } from "supertokens-node/lib/build/ingredients/smsdelivery/services/twilio";
 import type { TypeInput as PasswordlessRecipeConfig } from "supertokens-node/recipe/passwordless/types";
 
 const getPasswordlessRecipeConfig = (
@@ -15,9 +17,35 @@ const getPasswordlessRecipeConfig = (
     passwordless = config.user.supertokens.recipes.passwordless;
   }
 
+  if (!("messagingServiceSid" in config.twilio) && !("from" in config.twilio)) {
+    throw new Error(
+      "Twilio config requires either messagingServiceSid or from",
+    );
+  }
+
+  const twilioSettings: TwilioServiceConfig =
+    "messagingServiceSid" in config.twilio
+      ? {
+          opts: config.twilio.opts,
+          accountSid: config.twilio.accountSid,
+          authToken: config.twilio.authToken,
+          messagingServiceSid: config.twilio.messagingServiceSid,
+        }
+      : {
+          opts: config.twilio.opts,
+          accountSid: config.twilio.accountSid,
+          authToken: config.twilio.authToken,
+          from: config.twilio.from,
+        };
+
   return {
-    contactMethod: passwordless?.contactMethod || "EMAIL",
+    contactMethod: passwordless?.contactMethod || "PHONE",
     flowType: passwordless?.flowType || "USER_INPUT_CODE",
+    smsDelivery: {
+      service: new TwilioService({
+        twilioSettings,
+      }),
+    },
   };
 };
 

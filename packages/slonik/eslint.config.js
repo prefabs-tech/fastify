e3f0c34ce88423058bcd1fe5a51190642daee046
uddhab <uddhab@dzangolab.com>
2026-04-05 20:15:09 +0545

    chore: add perfectionist eslint plugin

--- packages/slonik/eslint.config.js
+++ packages/slonik/eslint.config.js
@@ -1,3 +1,22 @@
 import fastifyConfig from "@prefabs.tech/eslint-config/fastify.js";
+import perfectionist from "eslint-plugin-perfectionist";
 
-export default fastifyConfig;
+export default [
+  ...fastifyConfig,
+  {
+    plugins: {
+      perfectionist,
+    },
+    rules: {
+      // Disable conflicting default/import rules
+      "sort-imports": "off",
+      "import/order": "off",
+
+      // Enable and spread Perfectionist's recommended rules
+      ...perfectionist.configs["recommended-alphabetical"].rules,
+
+      // Add any Fastify-specific rule overrides here
+      "@typescript-eslint/no-explicit-any": "error",
+    },
+  },
+];

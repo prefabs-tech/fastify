91aef05c6e288fb29a73d9371d759c63343655b4
kabin thakuri <kabin@dzangolab.com>
2026-07-17 18:03:46 +0545

    chore(deps): update vite to v8

--- packages/error-handler/vite.config.ts
+++ packages/error-handler/vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig(({ mode }) => {
         formats: ["cjs", "es"],
         name: "PrefabsTechFastifyErrorHandler",
       },
-      rollupOptions: {
+      rolldownOptions: {
         external: [
           ...Object.keys(dependencies),
           ...Object.keys(peerDependencies),
--- packages/graphql/vite.config.ts
+++ packages/graphql/vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig(({ mode }) => {
         formats: ["cjs", "es"],
         name: "PrefabsTechFastifyGraphql",
       },
-      rollupOptions: {
+      rolldownOptions: {
         external: [
           ...Object.keys(dependencies),
           ...Object.keys(peerDependencies),
